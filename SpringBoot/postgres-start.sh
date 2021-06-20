08b03dcac63b2f0157448c4f6efabde2ff41e2c8
error23 <error23.d@gmail.com>
2021-06-20 16:50:57 +0200

    fix postgresql

--- SpringBoot/postgres-start.sh
+++ SpringBoot/postgres-start.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
+echo "localhost:5432:*:$DB_USER:$DB_PASSWORD" >~/.pgpass
+chmod 600 ~/.pgpass
+
 if [ ! -f pgsql/data/PG_VERSION ]; then
-  echo $DB_PASSWORD >pgsql/pass
-  initdb -D pgsql/data -E=UTF8 -U$DB_USER --locale=$DB_LOCALE -Amd5 --pwfile=pgsql/pass
-  rm pgsql/pass
-  unset DB_PASSWORD
-  unset DB_USER
+  echo $DB_PASSWORD >pgsql/password
+  initdb -D pgsql/data -E=UTF8 -U$DB_USER --locale=$LANG -Amd5 --pwfile=pgsql/password
+  rm pgsql/password
 fi
 
 pg_ctl -D pgsql/data -l log/postgresql start
+
+createdb -U$DB_USER --lc-collate=$LANG --lc-ctype=$LANG --template="template0" $DB_NAME
+
+for file in $(ls sql/ | sort); do
+  psql -d$DB_NAME -U$DB_USER <sql/$file
+done
+
+# clean up passwords
+unset DB_PASSWORD
+unset DB_USER
+rm ~/.pgpass
